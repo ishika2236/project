be602976dd53d93a7778d2eb0da8dba0365111c2
Ishika Gulati <135015217+ishika2236@users.noreply.github.com>
2025-05-21 21:00:05 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'https://project-i3sw.vercel.app',
+    origin: ['https://project-i3sw.vercel.app','https://project-bkly.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
 }));
